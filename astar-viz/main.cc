1a7f9dfc005ea5113a1cf4bd7565fa9c6fcb72e5
Kosi Nwabueze <kosinwabueze@gmail.com>
2018-05-21 15:00:40 -0700

    Added post-build script to copy SDL2.dll to output dir

--- astar-viz/main.cc
+++ astar-viz/main.cc
@@ -2,7 +2,19 @@
 
 #include <SDL.h>
 
-int main()
-{	
+int main(int argc, char** argv)
+{
+	SDL_Window *window = SDL_CreateWindow("A* Visualization by Kosi Nwabueze",
+		SDL_WINDOWPOS_UNDEFINED,
+		SDL_WINDOWPOS_UNDEFINED,
+		1280, 720, 0);
+
+	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);	
+	
+
+	SDL_DestroyWindow(window);
+	SDL_DestroyRenderer(renderer);
+	SDL_Quit();
+	
 	return 0;
 }
