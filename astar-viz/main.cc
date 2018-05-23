55afb6ecd9a9fa790b4918fc8134037a3d041298
Kosi Nwabueze <kosinwabueze@gmail.com>
2018-05-23 15:29:05 -0700

    Drew horizontal + vertical lines, fix spacing between lines

--- astar-viz/main.cc
+++ astar-viz/main.cc
@@ -18,8 +18,8 @@ struct Grid
 {
 	explicit Grid(int32_t x, int32_t y)
 		: X(x),
-		Y(y),
-		Points(x * y, CellState::NONE)
+		  Y(y),
+		  Points(x * y, CellState::NONE)
 	{
 	}
 
@@ -49,12 +49,43 @@ int32_t HandleEvents(SDL_Event &event)
 	return 0;
 }
 
-void RenderGrid(SDL_Renderer *rhnd, const Grid &grid)
-{
-	for (size_t i = 0; i < grid.X * grid.Y; ++i)
+void RenderGrid(SDL_Renderer *rhnd, const Grid &grid, const int32_t width, const int32_t height)
+{	
+	SDL_SetRenderDrawColor(rhnd, 0x0, 0x0, 0x0, 1);
+
+	int32_t hSpacing = width / grid.X;
+		
+	for (size_t x = 0; x < grid.X; ++x) // vertical lines
 	{
-		// draw a square
+		SDL_RenderDrawLine(rhnd, hSpacing * (int32_t)x, 0, hSpacing * (int32_t)x, height);
 	}
+
+	int32_t vSpacing = height / grid.Y;
+	for (size_t y = 0; y < grid.Y; ++y) // horizontal lines
+	{
+		SDL_RenderDrawLine(rhnd, 0, vSpacing * (int32_t)y, width, vSpacing * (int32_t)y);
+	}
+
+	for (size_t i = 0; i < grid.X * grid.Y; ++i) // potential squares
+	{		
+		const CellState &cellState = grid.Points[i];
+
+		// Draw grid lines
+
+		switch (cellState)
+		{
+		case CellState::GOAL: // draw red square
+			break;
+		case CellState::NONE: // don't draw anything else
+			break;
+		case CellState::SOURCE: // draw green square
+			break;
+		case CellState::VISITED: // draw orange square
+			break;
+		}
+	}
+
+	SDL_SetRenderDrawColor(rhnd, 0xFF, 0xFF, 0xFF, 1);
 }
 
 int main(int argc, char **argv)
@@ -66,15 +97,18 @@ int main(int argc, char **argv)
 		return -1;
 	}
 
-	SDL_Window *whnd = SDL_CreateWindow("A* Visualization", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1280, 720, 0);
-	SDL_Renderer *rhnd = SDL_CreateRenderer(whnd, -1, SDL_RENDERER_ACCELERATED);
+	const int32_t width = 1280;
+	const int32_t height = 720;
 
-	SDL_Event ev;
+	SDL_Window *whnd = SDL_CreateWindow("A* Visualization", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, 0);
+	SDL_Renderer *rhnd = SDL_CreateRenderer(whnd, -1, SDL_RENDERER_ACCELERATED);		
+
+	SDL_Event ev;	
 
 	Grid grid(25, 25);
 	A::Pathfinder pathfinder;
 
-	pathfinder.SetGraphSize((uint32_t)grid.X, (uint32_t)grid.Y);	
+	pathfinder.SetGraphSize((uint32_t)grid.X, (uint32_t)grid.Y);
 
 	bool running = true;
 
@@ -87,7 +121,10 @@ int main(int argc, char **argv)
 			running = false;
 		}
 
-		RenderGrid(rhnd, grid);
+		RenderGrid(rhnd, grid, width, height);
+		
+		SDL_RenderPresent(rhnd);
+		SDL_RenderClear(rhnd);
 	}
 
 	SDL_DestroyRenderer(rhnd);
