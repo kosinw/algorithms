ce4b81cb608dd2573b46293ca6e14e0a00572057
Kosi Nwabueze <kosinwabueze@gmail.com>
2018-05-23 14:54:05 -0700

    Deleted VS specific project files, completely CMake

--- astar-viz/main.cc
+++ astar-viz/main.cc
@@ -1,11 +1,61 @@
-#include "../astar/pathfind.hh"
+#include <astar/pathfind.hh>
 
 #include <iostream>
 #include <stdint.h>
+#include <vector>
 
 #include <SDL.h>
 
-#include "state.hh"
+enum CellState
+{
+	VISITED = 1,
+	SOURCE = 2,
+	GOAL = 3,
+	NONE = 0
+};
+
+struct Grid
+{
+	explicit Grid(int32_t x, int32_t y)
+		: X(x),
+		Y(y),
+		Points(x * y, CellState::NONE)
+	{
+	}
+
+	std::vector<CellState> Points;
+	int32_t X;
+	int32_t Y;
+};
+
+int32_t HandleEvents(SDL_Event &event)
+{
+	switch (event.type)
+	{
+	case SDL_QUIT:
+	{
+		return -1;
+	}
+	case SDL_KEYDOWN:
+	{
+		switch (event.key.keysym.sym)
+		{
+		case SDLK_ESCAPE:
+			return -1;
+		}
+	}
+	}
+
+	return 0;
+}
+
+void RenderGrid(SDL_Renderer *rhnd, const Grid &grid)
+{
+	for (size_t i = 0; i < grid.X * grid.Y; ++i)
+	{
+		// draw a square
+	}
+}
 
 int main(int argc, char **argv)
 {
@@ -20,6 +70,29 @@ int main(int argc, char **argv)
 	SDL_Renderer *rhnd = SDL_CreateRenderer(whnd, -1, SDL_RENDERER_ACCELERATED);
 
 	SDL_Event ev;
+
+	Grid grid(25, 25);
+	A::Pathfinder pathfinder;
+
+	pathfinder.SetGraphSize((uint32_t)grid.X, (uint32_t)grid.Y);	
+
+	bool running = true;
+
+	while (running)
+	{
+		SDL_PollEvent(&ev);
+
+		if (HandleEvents(ev) == -1)
+		{
+			running = false;
+		}
+
+		RenderGrid(rhnd, grid);
+	}
+
+	SDL_DestroyRenderer(rhnd);
+	SDL_DestroyWindow(whnd);
+	SDL_Quit();
 
 	return 0;
 }
