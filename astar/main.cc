d6e897124a1cec2f5ca6766cae6928802550d30a
Kosi Nwabueze <kosinwabueze@gmail.com>
2018-05-26 22:34:11 -0700

    Decided to shelve GUI project

---
+++ astar/main.cc
@@ -0,0 +1,11 @@
+#include "pathfind.hh"
+
+#include <iostream>
+#include <vector>
+#include <stdlib.h>
+#include <Windows.h>
+
+int main()
+{
+	MessageBoxW(0, L"Press OK", L"Hi", MB_SETFOREGROUND);
+}
